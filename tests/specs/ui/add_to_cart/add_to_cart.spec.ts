186c38ee666f61f4ab02bc1bf5070493d055059a
Mohti Jangra <mohitjangra6017@gmail.com>
2023-09-23 11:44:18 +0530

    add for taniya

--- tests/specs/ui/add_to_cart/add_to_cart.spec.ts
+++ tests/specs/ui/add_to_cart/add_to_cart.spec.ts
@@ -2,7 +2,6 @@
 import { test, APIRequestContext, Page } from '@playwright/test'
 import { LoginPage } from '../../../pages/login/login.page'
 import { CommonPage } from '../../../pages/common/common.page'
-import { logoutData } from '../../../testdata/ui/logout.data'
 import { add_to_cart_data } from '../../../testdata/ui/add_to_cart.data'
 import { Add_To_Cart_Page } from '../../../pages/add_to_cart/add_to_cart.page'
 
@@ -10,14 +9,18 @@ let request: APIRequestContext
 let page: Page
 let commonPage: CommonPage
 let add_to_cart: Add_To_Cart_Page;
+let loginPage: LoginPage
 
 test.beforeAll(async ({ browser }) => {
     page = await browser.newPage()
     request = (await browser.newContext()).request
     commonPage = new CommonPage(page)
     add_to_cart = new Add_To_Cart_Page(page)
+    loginPage = new LoginPage(page)
     await page.goto('/demo')
     await commonPage.pageLoadCheck()
+    await loginPage.userLoggedIn();
+    
 })
 add_to_cart_data.forEach(data => {
     test.describe.parallel(`Verify the working of Register Account functionality`, async () => {
